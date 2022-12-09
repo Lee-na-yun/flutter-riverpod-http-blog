df6919c1a08a702c676766b95648b3384586cb7a
Lee-na-yun <happyin3333@nate.com>
2022-12-09 11:37:40 +0900

    기riverpod 기본 세팅 완료

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:blog/core/routers.dart';
+import 'package:blog/view/pages/post/detail_page.dart';
+import 'package:blog/view/pages/user/join_page.dart';
+import 'package:blog/view/pages/user/login_page.dart';
+import 'package:blog/view/pages/post/update_page.dart';
+import 'package:blog/view/pages/post/write_page.dart';
+import 'package:blog/view/pages/user/user_info_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'view/pages/post/home_page.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+final navigatorKey = GlobalKey<NavigatorState>();
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      debugShowCheckedModeBanner: false,
+
+      // 라우트 설계 필요없음. GetX 사용할 예정
+      initialRoute: Routers.home,
+      routes: {
+        Routers.home: (context) => HomePage(),
+        Routers.detail: (context) => DetailPage(),
+        Routers.updateForm: (context) => UpdatePage(),
+        Routers.joinForm: (context) => JoinPage(),
+        Routers.loginForm: (context) => LoginPage(),
+        Routers.writeForm: (context) => WritePage(),
+        Routers.userInfo: (context) => UserInfoPage(),
+      },
+    );
+  }
+}
