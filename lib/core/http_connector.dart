c865c2b8789fee245e26c4b4fb454cd15d16f218
Lee-na-yun <happyin3333@nate.com>
2022-12-09 11:57:15 +0900

    toJson수정 및 개념잡기

--- lib/core/http_connector.dart
+++ lib/core/http_connector.dart
@@ -2,8 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart';
 
 final httpConnector = Provider<HttpConnector>((ref) {
-  return HttpConnector();
-});
+  //함수를 담고 있음
+  return HttpConnector(); // lead 또는 watch할 때 메모리에 뜨게 됨 (최초에 안뜸!)
+}); // 기존 new된걸 쓰는 싱글톤으로 유지하려고 사용함!! (* 필요할때마다 필요한곳에서 new해서 써도 됨)
 
 class HttpConnector {
   final host = "http://localhost:8080";
